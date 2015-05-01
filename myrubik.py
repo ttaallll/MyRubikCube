2040519e1de46c8e0767429ab6496293211a8176
tal <tal.pais@cmycasa.com>
2015-05-01 19:17:32 +0300

    add rotation

--- myrubik.py
+++ myrubik.py
@@ -3,34 +3,77 @@ __author__ = 'tal'
 
 
 cube = {
-    'up': [[0,0,0,],
-     [0,0,0],
-     [0,0,0]], # Up
+    'up': [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ], # Up
 
-    'down': [1,1,1,
-     1,1,1,
-     1,1,1], # Down
+    'down': [
+        [1,1,1],
+        [1,1,1],
+        [1,1,1]
+    ], # Down
 
-    'left': [2,2,2,
-     2,2,2,
-     2,2,2], # Left
+    'left': [
+        [2,2,2],
+        [2,2,2],
+        [2,2,2]
+    ], # Left
 
-    'right': [3,3,3,
-     3,3,3,
-     3,3,3], # Right
+    'right': [
+        [3,3,3],
+        [3,3,3],
+        [3,3,3]
+    ], # Right
 
-    'front': [4,4,4,
-     4,4,4,
-     4,4,4], # Front
+    'front': [
+        [4,4,4],
+        [4,4,4],
+        [4,4,4]
+    ], # Front
 
-    'back': [5,5,5,
-     5,5,5,
-     5,5,5]  # Back
+    'back': [
+        [5,5,5],
+        [5,5,5],
+        [5,5,5]
+    ]  # Back
 }
 
 
 def rotateLeft(side):
-    side[]
+
+    oldside = side[:]
+
+    side[0][0] = oldside[0][2]
+    side[0][1] = oldside[1][2]
+    side[0][2] = oldside[2][2]
+
+    side[1][2] = oldside[2][1]
+    side[2][2] = oldside[2][0]
+
+    side[2][1] = oldside[1][0]
+    side[2][0] = oldside[0][0]
+
+    side[1][0] = oldside[0][1]
+
+
+def rotateRight(side):
+
+    oldside = side[:]
+
+    side[0][0] = oldside[2][0]
+    side[0][1] = oldside[1][0]
+    side[0][2] = oldside[0][0]
+
+    side[1][2] = oldside[0][1]
+    side[2][2] = oldside[0][2]
+
+    side[2][1] = oldside[1][2]
+    side[2][0] = oldside[2][2]
+
+    side[1][0] = oldside[2][1]
+
 
 def F2R1(cube):
 
